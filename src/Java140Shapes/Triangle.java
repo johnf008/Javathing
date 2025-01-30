3b23a12b3f9f2f31c17c9517c1396412a3ce7b67
johnf008 <Carlos.flores.h108@gmail.com>
2025-01-30 10:51:46 -0600

    add everything i didn't add

---
+++ src/Java140Shapes/Triangle.java
@@ -0,0 +1,9 @@
+package Java140Shapes;
+//programmerJohn
+//contains the Triangle class and its methods
+public class Triangle {
+
+    public void getArea() {
+        System.out.println("The are of the triangle...");
+    }
+}
